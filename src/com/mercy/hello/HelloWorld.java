7c6eee5683a18e4a0a4173243b8c87685e239ea1
kun24 <kun24@outlook.com>
2018-03-17 16:42:31 +0800

    add AAA

--- src/com/mercy/hello/HelloWorld.java
+++ src/com/mercy/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package com.mercy.hello;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!AAA");
 	}
 
 }
